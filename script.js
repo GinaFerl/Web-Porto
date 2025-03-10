b82e2c1bb34fe38273e2267459cbb5e102305585
GinaFerl <ginaferlindawati@gmail.com>
2025-03-10 09:54:03 +0700

    add project

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector('#menu-icon');
     const navLinks = document.querySelector('.nav-links');
+    const githubButton = document.querySelector('.visit-btn');
 
     menuIcon.onclick = function () {
         navLinks.classList.toggle('active');
+    };
+    githubButton.onclick = function () {
+        window.open("https://github.com/GinaFerl", "_blank"); 
     };
 });
